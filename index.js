f647185f7a04e9acadbd8815d45806ccee48ddf1
Nazar <rusetskiy2788@mail.ru>
2022-03-03 17:24:43 +0300

    fix bug

--- index.js
+++ index.js
@@ -18,24 +18,33 @@ const removeUser = (socketId) => {
 const getUser = (userId) => {
     return users.find((user) => user.userId === userId);
 };
-
 io.on("connection", (socket) => {
     //when ceonnect
     console.log("a user connected.");
-
+    
     //take userId and socketId from user
     socket.on("addUser", (userId) => {
+        console.log("line 27 userId",userId)
         addUser(userId, socket.id);
         io.emit("getUsers", users);
     });
 
     //send and get message
     socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-        const user = getUser(receiverId);
-        io.to(user.socketId).emit("getMessage", {
-            senderId,
-            text,
-        });
+        try{
+            console.log("line 34 senderId,receiverId,text",senderId, receiverId, text)
+            console.log("line 35 users",users)
+            const user = getUser(receiverId);
+            console.log("line 37 user", user)
+            io.to(user.socketId).emit("getMessage", {
+                senderId,
+                text,
+            });
+        }
+        catch(err){
+            console.log("line 45", err)
+        }
+
     });
 
     //when disconnect
